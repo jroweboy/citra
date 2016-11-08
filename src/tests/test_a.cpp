e911549780cce963e8e26153c8cd6ee60016b960
wwylele <wwylele@gmail.com>
2016-11-08 21:07:21 +0200

    Add a dummy test

---
+++ src/tests/test_a.cpp
@@ -0,0 +1,13 @@
+// Copyright 2016 Citra Emulator Project
+// Licensed under GPLv2 or any later version
+// Refer to the license.txt file included.
+
+#include <catch.hpp>
+
+namespace FileSys {
+
+TEST_CASE("PathParser", "[core][file_sys]") {
+    REQUIRE(1 + 1 == 2);
+}
+
+} // namespace FileSys
