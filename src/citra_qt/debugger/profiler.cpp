6bae421020faec0453025b7042559f29ab9bd0cb
emmaus <emmausssss@gmail.com>
2016-11-10 19:05:10 +0000

    split fps

--- src/citra_qt/debugger/profiler.cpp
+++ src/citra_qt/debugger/profiler.cpp
@@ -42,6 +42,9 @@ static QVariant GetFpsDataForColumn(int col, const AggregatedDuration& duration)
     static auto get_fps = [](Duration dur) -> float {
         using FloatMs = std::chrono::duration<float, std::chrono::milliseconds::period>;
         float fps = 1000/std::chrono::duration_cast<FloatMs>(dur).count();
+        if (dur.count() == 0) {
+            fps = 0;
+        }
         return fps;
     };
 
@@ -94,7 +97,7 @@ int ProfilerModel::rowCount(const QModelIndex& parent) const {
     if (parent.isValid()) {
         return 0;
     } else {
-        return 3;
+        return 4;
     }
 }
 
@@ -103,18 +106,30 @@ QVariant ProfilerModel::data(const QModelIndex& index, int role) const {
         if (index.row() == 0) {
             if (index.column() == 0) {
                 return tr("Frame");
-            } else {
+            }
+            else {
                 return GetDataForColumn(index.column(), results.frame_time);
             }
-        } else if (index.row() == 1) {
+        }
+        else if (index.row() == 1) {
             if (index.column() == 0) {
                 return tr("Frame (with swapping)");
-            } else {
+            }
+            else {
                 return GetDataForColumn(index.column(), results.interframe_time);
             }
-        } else if (index.row() == 2) {
+        }
+        else if (index.row() == 2) {
             if (index.column() == 0) {
                 return tr("Frame per second, fps");
+            }
+            else {
+                return GetDataForColumn(index.column(), results.frame_time);
+            }
+        }
+        else if (index.row() == 3) {
+            if (index.column() == 0) {
+                return tr("Frame per second (with swapping), fps(vsync)");
             }
             else {
                 return GetFpsDataForColumn(index.column(), results.interframe_time);
